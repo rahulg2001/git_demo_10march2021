19b77742827097fdd452d156b40a6d964ebb033a
Rahul Ghiya <rahul20_01@yahoo.com>
2021-03-06 19:22:24 +0000

    modified Readme to add 3 and added Book

---
+++ Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public  class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
